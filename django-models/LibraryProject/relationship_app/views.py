9b80d697ec9639a1454c62ffd7955ba354381ba4
tinbit25 <tinbitelias25@gmail.com>
2025-02-23 19:41:16 +0300

    Added function-based and class-based views for books and libraries

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Library
 
-# Create your views here.
+# Class-based view to display details of a specific library
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = "library_detail.html"
+    context_object_name = "library"
