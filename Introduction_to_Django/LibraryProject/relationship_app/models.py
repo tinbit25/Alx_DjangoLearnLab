a90b9fe1e79344413abb94e859af835a2683f90c
tinbit25 <tinbitelias25@gmail.com>
2025-02-23 16:34:52 +0300

    Implement advanced model relationships and sample queries

--- Introduction_to_Django/LibraryProject/relationship_app/models.py
+++ Introduction_to_Django/LibraryProject/relationship_app/models.py
@@ -1,13 +1,11 @@
 from django.db import models
 
-# Author model
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
-# Book model with a ForeignKey to Author
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
@@ -15,15 +13,13 @@ class Book(models.Model):
     def __str__(self):
         return self.title
 
-# Library model
 class Library(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=200)
     books = models.ManyToManyField(Book)
 
     def __str__(self):
         return self.name
 
-# Librarian model with a OneToOneField to Library
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
