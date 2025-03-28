3c3f6e9617c8d3d286783bf86cb0e4fa78b96fd6
tinbit25 <tinbitelias25@gmail.com>
2025-03-28 21:54:35 +0300

    update

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,62 +1,20 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import CreateView, UpdateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from .models import Post, Comment
-from .forms import CommentForm
-from django.urls import reverse_lazy
+from django.shortcuts import render
+from .models import Post
 
-# Display comments for a specific post and allow users to add new comments
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    comments = post.comments.all()  # Get all comments related to the post
-    if request.method == 'POST' and request.user.is_authenticated:
-        form = CommentForm(request.POST)
-        if form.is_valid():
-            comment = form.save(commit=False)
-            comment.post = post
-            comment.author = request.user
-            comment.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = CommentForm()
+def post_search(request):
+    query = request.GET.get('q', '')  # Get search query from URL parameter 'q'
     
-    return render(request, 'blog/post_detail.html', {'post': post, 'comments': comments, 'form': form})
-
-# Create a new comment
-class CommentCreateView(LoginRequiredMixin, CreateView):
-    model = Comment
-    form_class = CommentForm
-    template_name = 'blog/comment_form.html'
-
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        form.instance.post = get_object_or_404(Post, pk=self.kwargs['pk'])  # Get the post this comment belongs to
-        return super().form_valid(form)
-
-    def get_success_url(self):
-        return reverse_lazy('post_detail', kwargs={'pk': self.object.post.pk})
-
-# Edit a comment (only the author can edit it)
-class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-    model = Comment
-    fields = ['content']
-    template_name = 'blog/comment_form.html'
-
-    def test_func(self):
-        comment = self.get_object()
-        return comment.author == self.request.user
-
-    def get_success_url(self):
-        return reverse_lazy('post_detail', kwargs={'pk': self.get_object().post.pk})
-
-# Delete a comment (only the author can delete it)
-class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
-    model = Comment
-    template_name = 'blog/comment_confirm_delete.html'
-
-    def test_func(self):
-        comment = self.get_object()
-        return comment.author == self.request.user
-
-    def get_success_url(self):
-        return reverse_lazy('post_detail', kwargs={'pk': self.get_object().post.pk})
+    # Filter posts based on the query, matching title, tags, or content
+    posts = Post.objects.filter(
+        title__icontains=query  # Search by title (case-insensitive)
+    )
+    
+    # If a query is present, also filter by tags and content
+    if query:
+        posts = posts.filter(
+            title__icontains=query | 
+            tags__name__icontains=query |  # Search by tags (case-insensitive)
+            content__icontains=query  # Search by content (case-insensitive)
+        )
+    
+    return render(request, 'post_search_results.html', {'posts': posts, 'query': query})
