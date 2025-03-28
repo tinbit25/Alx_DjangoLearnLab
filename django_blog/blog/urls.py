8f3da2034be8723bd126fd8c96bca757dfed6b1e
tinbit25 <tinbitelias25@gmail.com>
2025-03-28 21:32:04 +0300

    update las

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
     # Blog post URLs
     path('posts/<int:pk>/', views.post_detail, name='post_detail'),  # View a specific post
-    path('posts/<int:pk>/comments/new/', views.CommentCreateView.as_view(), name='comment_new'),  # Create a new comment
 
-    # Comment update and delete URLs
-    path('comments/<int:pk>/edit/', views.CommentUpdateView.as_view(), name='comment_edit'),  # Edit an existing comment
+    # Comment related URLs
+    path('posts/<int:pk>/comments/new/', views.CommentCreateView.as_view(), name='comment_new'),  # Create a new comment
+    path('comments/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment_update'),  # Update a comment
     path('comments/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment_delete'),  # Delete a comment
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,62 +1,42 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import get_object_or_404
 from django.views.generic import CreateView, UpdateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from .models import Post, Comment
-from .forms import CommentForm
 from django.urls import reverse_lazy
+from .models import Comment, Post
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Display comments for a specific post and allow users to add new comments
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    comments = post.comments.all()  # Get all comments related to the post
-    if request.method == 'POST' and request.user.is_authenticated:
-        form = CommentForm(request.POST)
-        if form.is_valid():
-            comment = form.save(commit=False)
-            comment.post = post
-            comment.author = request.user
-            comment.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = CommentForm()
-    
-    return render(request, 'blog/post_detail.html', {'post': post, 'comments': comments, 'form': form})
-
-# Create a new comment
+# View to create a new comment
 class CommentCreateView(LoginRequiredMixin, CreateView):
-    model = Comment
-    form_class = CommentForm
-    template_name = 'blog/comment_form.html'
-
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        form.instance.post = get_object_or_404(Post, pk=self.kwargs['pk'])  # Get the post this comment belongs to
-        return super().form_valid(form)
-
-    def get_success_url(self):
-        return reverse_lazy('post_detail', kwargs={'pk': self.object.post.pk})
-
-# Edit a comment (only the author can edit it)
-class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Comment
     fields = ['content']
     template_name = 'blog/comment_form.html'
 
-    def test_func(self):
-        comment = self.get_object()
-        return comment.author == self.request.user
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.instance.post = get_object_or_404(Post, pk=self.kwargs['pk'])
+        return super().form_valid(form)
 
     def get_success_url(self):
-        return reverse_lazy('post_detail', kwargs={'pk': self.get_object().post.pk})
+        return reverse_lazy('post_detail', kwargs={'pk': self.kwargs['pk']})
 
-# Delete a comment (only the author can delete it)
-class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+# View to update a comment
+class CommentUpdateView(LoginRequiredMixin, UpdateView):
+    model = Comment
+    fields = ['content']
+    template_name = 'blog/comment_form.html'
+
+    def get_queryset(self):
+        return Comment.objects.filter(author=self.request.user)
+
+    def get_success_url(self):
+        return reverse_lazy('post_detail', kwargs={'pk': self.object.post.pk})
+
+# View to delete a comment
+class CommentDeleteView(LoginRequiredMixin, DeleteView):
     model = Comment
     template_name = 'blog/comment_confirm_delete.html'
 
-    def test_func(self):
-        comment = self.get_object()
-        return comment.author == self.request.user
+    def get_queryset(self):
+        return Comment.objects.filter(author=self.request.user)
 
     def get_success_url(self):
-        return reverse_lazy('post_detail', kwargs={'pk': self.get_object().post.pk})
+        return reverse_lazy('post_detail', kwargs={'pk': self.object.post.pk})
