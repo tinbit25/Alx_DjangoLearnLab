ae45c4e64645e51ccc7b99bfcbaa6dcecd44b399
tinbit25 <tinbitelias25@gmail.com>
2025-03-28 21:53:18 +0300

    update

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -17,3 +17,15 @@ class CommentForm(forms.ModelForm):
         widgets = {
             'content': forms.Textarea(attrs={'rows': 4, 'cols': 40}),
         }
+from django import forms
+from .models import Post
+from taggit.forms import TagWidget  # This is necessary to create the Tag widget
+
+class PostForm(forms.ModelForm):
+    tags = forms.CharField(
+        widget=TagWidget()  # This adds a tag input widget
+    )
+
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'tags']  # Include 'tags' in the form fields
