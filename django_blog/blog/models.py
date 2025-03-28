037a0fa6fa1837ef4dd5fddadea2a5679be4f460
tinbit25 <tinbitelias25@gmail.com>
2025-03-28 21:40:31 +0300

    update

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -2,12 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=100)
     content = models.TextField()
-    created_at = models.DateTimeField(default=timezone.now)
-    updated_at = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    tags = models.ManyToManyField(Tag, related_name='posts', blank=True)  # Add this line
 
     def __str__(self):
         return self.title
