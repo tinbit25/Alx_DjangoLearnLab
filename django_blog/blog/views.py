a16aa28af16c88aea2834b0f364e3553b0d2d7b9
tinbit25 <tinbitelias25@gmail.com>
2025-03-28 21:21:32 +0300

    update las

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,52 +1,51 @@
-# blog/views.py
-
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
+from .forms import PostForm
 
-# List all posts
+# List View for all blog posts
 class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
 
-# View a single post
+# Detail View for a specific post
 class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
     context_object_name = 'post'
 
-# Create a new post
-class PostCreateView(LoginRequiredMixin, CreateView):
+# Create View for a new post (requires login)
+@login_required
+class PostCreateView(CreateView):
     model = Post
+    form_class = PostForm
     template_name = 'blog/post_form.html'
-    fields = ['title', 'content']
-
+    
     def form_valid(self, form):
         form.instance.author = self.request.user  # Set the author to the logged-in user
         return super().form_valid(form)
 
-# Update an existing post
-class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+# Update View for an existing post (requires login and owner check)
+@login_required
+class PostUpdateView(UpdateView):
     model = Post
+    form_class = PostForm
     template_name = 'blog/post_form.html'
-    fields = ['title', 'content']
 
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        return super().form_valid(form)
+    def get_queryset(self):
+        # Ensure that users can only edit their own posts
+        return Post.objects.filter(author=self.request.user)
 
-    def test_func(self):
-        post = self.get_object()
-        return post.author == self.request.user
-
-# Delete a post
-class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+# Delete View for a post (requires login and owner check)
+@login_required
+class PostDeleteView(DeleteView):
     model = Post
     template_name = 'blog/post_confirm_delete.html'
     success_url = reverse_lazy('blog:post_list')
 
-    def test_func(self):
-        post = self.get_object()
-        return post.author == self.request.user
+    def get_queryset(self):
+        # Ensure that users can only delete their own posts
+        return Post.objects.filter(author=self.request.user)
