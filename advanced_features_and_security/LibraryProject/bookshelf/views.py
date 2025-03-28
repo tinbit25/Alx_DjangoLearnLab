67066649545d08afbaaddc2439108587db7966c0
tinbit25 <tinbitelias25@gmail.com>
2025-03-28 10:17:30 +0300

    Implement custom permissions and groups

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+# views.py
 
-# Create your views here.
+from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from .models import Book
+
+# List of books with permission check
+@permission_required('books.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()  # Retrieve all books from the database
+    return render(request, 'book_list.html', {'books': books})
