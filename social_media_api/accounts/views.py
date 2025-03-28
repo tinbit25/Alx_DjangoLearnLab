e4d8ca88f08548465c548effcfd09b64e952bade
tinbit25 <tinbitelias25@gmail.com>
2025-03-28 22:35:10 +0300

    social media

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -2,12 +2,12 @@ from rest_framework import generics, permissions
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.shortcuts import get_object_or_404
-from .models import User
+from .models import CustomUser  # Assuming your custom user model is named CustomUser
 from rest_framework.permissions import IsAuthenticated
 
 # To list all users (using GenericAPIView)
 class UserListView(generics.ListAPIView):
-    queryset = User.objects.all()  # Fetch all users from the database
+    queryset = CustomUser.objects.all()  # Fetch all users from the database
     permission_classes = [IsAuthenticated]  # Ensure that the user is authenticated
 
     def get(self, request, *args, **kwargs):
@@ -23,7 +23,7 @@ class UserListView(generics.ListAPIView):
 # Follow and Unfollow views
 @api_view(['POST'])
 def follow_user(request, user_id):
-    user_to_follow = get_object_or_404(User, id=user_id)
+    user_to_follow = get_object_or_404(CustomUser, id=user_id)
     if user_to_follow == request.user:
         return Response({"message": "You cannot follow yourself."}, status=400)
     request.user.following.add(user_to_follow)
@@ -32,6 +32,6 @@ def follow_user(request, user_id):
 
 @api_view(['POST'])
 def unfollow_user(request, user_id):
-    user_to_unfollow = get_object_or_404(User, id=user_id)
+    user_to_unfollow = get_object_or_404(CustomUser, id=user_id)
     request.user.following.remove(user_to_unfollow)
     return Response({'message': 'You have unfollowed this user.'})
