36728521b2e00ab7039f3aa150bd66864a9b73ae
tinbit25 <tinbitelias25@gmail.com>
2025-03-28 20:10:43 +0300

    update last

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('register/', views.register, name='register'),
-    path('profile/', views.profile, name='profile'),
+    path("register/", views.register, name="register"),
+    path("login/", views.login_user, name="login"),
+    path("logout/", views.logout_user, name="logout"),
+    path("profile/", views.profile, name="profile"),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,15 +1,40 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.contrib.auth import login, logout, authenticate
 from .forms import UserRegisterForm
 
 def register(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = UserRegisterForm(request.POST)
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('login')
+            user = form.save()
+            login(request, user)
+            messages.success(request, f"Account created successfully!")
+            return redirect("profile")
     else:
         form = UserRegisterForm()
-    return render(request, 'users/register.html', {'form': form})
+    return render(request, "users/register.html", {"form": form})
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Logged in successfully!")
+            return redirect("profile")
+        else:
+            messages.error(request, "Invalid credentials!")
+    return render(request, "users/login.html")
+
+@login_required
+def logout_user(request):
+    logout(request)
+    messages.success(request, "Logged out successfully!")
+    return redirect("login")
+
+@login_required
+def profile(request):
+    return render(request, "users/profile.html")
