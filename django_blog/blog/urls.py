a276b215c4e0100c5869582f8301c07293798361
tinbit25 <tinbitelias25@gmail.com>
2025-03-28 12:45:20 +0300

    create djangoblog

---
+++ django_blog/blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
+]
