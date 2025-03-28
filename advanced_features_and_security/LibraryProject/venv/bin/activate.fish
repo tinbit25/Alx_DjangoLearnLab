07d8072d0d407fed5abae15c982f6b8622608673
tinbit25 <tinbitelias25@gmail.com>
2025-03-28 09:51:15 +0300

    final

--- advanced_features_and_security/LibraryProject/venv/bin/activate.fish
+++ advanced_features_and_security/LibraryProject/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/tinbit/Alx_DjangoLearnLab/advanced_features_and_security/venv
+set -gx VIRTUAL_ENV /home/tinbit/Alx_DjangoLearnLab/advanced_features_and_security/LibraryProject/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
