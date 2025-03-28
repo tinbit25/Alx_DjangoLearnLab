c4708dd889dcbdb2f25083441c28abbde3126676
tinbit25 <tinbitelias25@gmail.com>
2025-03-28 21:34:58 +0300

    update

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Blog post URLs
+    # Post URLs
     path('posts/<int:pk>/', views.post_detail, name='post_detail'),  # View a specific post
 
     # Comment related URLs
-    path('posts/<int:pk>/comments/new/', views.CommentCreateView.as_view(), name='comment_new'),  # Create a new comment
-    path('comments/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment_update'),  # Update a comment
-    path('comments/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment_delete'),  # Delete a comment
+    path('post/<int:pk>/comments/new/', views.CommentCreateView.as_view(), name='comment_new'),  # Create a new comment
+    path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment_update'),  # Update a comment
+    path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment_delete'),  # Delete a comment
 ]
