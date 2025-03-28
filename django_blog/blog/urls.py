68b739b96c0036ad7114ce0c6657dcc919bcaa68
tinbit25 <tinbitelias25@gmail.com>
2025-03-28 21:16:06 +0300

    update las

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -9,9 +9,9 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('profile/', views.profile, name='profile'),
- path('', views.PostListView.as_view(), name='post_list'),
-    path('posts/new/', views.PostCreateView.as_view(), name='post_create'),
-    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
-    path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
-    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
+  path('', views.PostListView.as_view(), name='post_list'),
+    path('posts/new/', views.PostCreateView.as_view(), name='post_create'),  # Correct path for creating posts
+    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),  # Correct path for post detail
+    path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),  # Correct path for editing posts
+    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),  # Correct path for deleting posts
 ]
