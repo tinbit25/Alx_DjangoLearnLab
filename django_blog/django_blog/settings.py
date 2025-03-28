20635ecb5242a5c21fbce474a25096010ad6ed89
tinbit25 <tinbitelias25@gmail.com>
2025-03-28 20:06:35 +0300

    update last

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -81,6 +81,10 @@ DATABASES = {
     }
 }
 
+# Override if DATABASE_URL exists (for cloud deployment)
+if os.getenv('DATABASE_URL'):
+    import dj_database_url
+    DATABASES['default'] = dj_database_url.config(default=os.getenv('DATABASE_URL'))
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
