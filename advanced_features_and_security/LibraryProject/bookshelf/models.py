fb366cc3a120d49ac9d176fdd4ccc2a81e40e7cc
tinbit25 <tinbitelias25@gmail.com>
2025-03-28 10:08:18 +0300

    final

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -5,5 +5,7 @@ class CustomUser(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
     profile_photo = models.ImageField(upload_to="profile_photos/", null=True, blank=True)
 
+    objects = CustomUserManager()  # Link the Custom User Manager
+
     def __str__(self):
         return self.username
