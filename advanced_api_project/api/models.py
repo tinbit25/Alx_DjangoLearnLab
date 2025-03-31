340394fcea2d6ae1d4802ad6eaf134b599e432be
tinbit25 <tinbitelias25@gmail.com>
2025-03-31 17:36:04 +0300

    ju

--- advanced_api_project/api/models.py
+++ advanced_api_project/api/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 
 class Author(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=100)
+    bio = models.TextField()
 
     def __str__(self):
         return self.name
 
+
 class Book(models.Model):
-    title = models.CharField(max_length=255)
-    publication_year = models.IntegerField()
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
+    published_date = models.DateField()
 
     def __str__(self):
         return self.title
--- advanced_api_project/api/serializers.py
+++ advanced_api_project/api/serializers.py
@@ -1,16 +1,15 @@
-# serializers.py
 from rest_framework import serializers
 from .models import Author, Book
 
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
-        fields = ['id', 'name', 'bio']
+        fields = '__all__'
 
 
 class BookSerializer(serializers.ModelSerializer):
-    author = AuthorSerializer()  # Nested Author serializer
+    author = AuthorSerializer()  # Nested serialization
 
     class Meta:
         model = Book
-        fields = ['id', 'title', 'author', 'published_date']
+        fields = '__all__'
