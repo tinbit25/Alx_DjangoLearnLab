2a050c2bf990f69a2d171b00b8310aca2b09d411
tinbit25 <tinbitelias25@gmail.com>
2025-03-31 20:21:24 +0300

    filter

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, filters
+from django_filters import rest_framework
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Book
 from .serializers import BookSerializer
