15ee48dacdffe9abeacc2072412b06e26640e3db
tinbit25 <tinbitelias25@gmail.com>
2025-03-31 20:53:45 +0300

    blog bug

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
 
+from django.contrib.auth import views as auth_views
+
+
 urlpatterns = [
-    # Post URLs
+     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
      path('tags/<slug:tag_slug>/', views.PostByTagListView.as_view(), name='post_by_tag'),
     path('posts/<int:pk>/', views.post_detail, name='post_detail'),  # View a specific post
 
