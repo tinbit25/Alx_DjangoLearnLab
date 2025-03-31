e677c5cea29b20f5b83234fdafa2e85429e4269f
tinbit25 <tinbitelias25@gmail.com>
2025-03-31 16:56:50 +0300

    correct

--- advanced_api_project/advanced_api_project/settings.py
+++ advanced_api_project/advanced_api_project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',  # Add Django REST Framework
-    'api',  # Add your app
+    'api',
 ]
 
 MIDDLEWARE = [
